2c75bf5df4ebfae3f5d3529fee2c99cd5ec85f8a
Maekiee <pdwssf@gmail.com>
2025-12-27 15:14:22 +0900

    코디네이터 추가

---
+++ Cachu/App/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Cachu
+//
+//  Created by 박도원 on 12/27/25.
+//
+
+import Foundation
---
+++ Cachu/App/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  Cachu
+//
+//  Created by 박도원 on 12/27/25.
+//
+
+import Foundation
