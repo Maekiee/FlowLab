a75f28bcd3db0ebb5e96c39962bf2c70c78b59b6
Maekiee <pdwssf@gmail.com>
2026-01-04 14:48:52 +0900

    end point 수정

--- Cachu/Network/APIEndpoint.swift
+++ Cachu/Network/APIEndpoint.swift
@@ -3,7 +3,6 @@ import Foundation
 
 enum APIEndpoint: Endpoint {
     case login(LoginRequestDTO)
-    case getProfile
     case join(JoinRequestDTO)
     
     var baseURL: URL {
@@ -13,7 +12,6 @@ enum APIEndpoint: Endpoint {
     var path: String {
         switch self {
         case .login: return "/users/login"
-        case .getProfile: return "/profile"
         case . join: return "/users/join"
         }
     }
@@ -21,7 +19,6 @@ enum APIEndpoint: Endpoint {
     var method: HTTPMethod {
         switch self {
         case .login, .join: return .post
-        case .getProfile: return .get
         }
     }
     
@@ -29,8 +26,6 @@ enum APIEndpoint: Endpoint {
         switch self {
         case .login(let loginDTO):
             return try? JSONEncoder().encode(loginDTO)
-        case .getProfile:
-            return nil
         case .join(let joinDTO):
             return try? JSONEncoder().encode(joinDTO)
         }
@@ -40,7 +35,6 @@ enum APIEndpoint: Endpoint {
     var requiresAuth: Bool {
         switch self {
         case .login, .join(_): return false // 로그인엔 토큰 필요 없음
-        case .getProfile: return true
         }
     }
 }
