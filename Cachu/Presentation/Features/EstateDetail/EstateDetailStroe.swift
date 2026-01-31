40fee98e6ec1911ce92a55fd6abf1f95b04837e6
Maekiee <pdwssf@gmail.com>
2026-01-31 18:19:27 +0900

    매물 상세 화면 api 연결

--- Cachu/Presentation/Features/EstateDetail/EstateDetailStroe.swift
+++ Cachu/Presentation/Features/EstateDetail/EstateDetailStroe.swift
@@ -7,33 +7,59 @@ import Combine
 final class EstateDetailStore: StoreProtocol {
     private(set) var state = State()
     private let repository: EstateDetailRepositoryProtocol
+    private let effectSubject = PassthroughSubject<SideEffect, Never>()
+    var effect: AnyPublisher<SideEffect, Never> {
+        effectSubject.eraseToAnyPublisher()
+    }
     
     init(
         repository: EstateDetailRepositoryProtocol,
         estateId: String
     ) {
         self.repository = repository
-        self.state.stateId = estateId
+        self.state.estateId = estateId
     }
     
     struct State {
-        var stateId: String = ""
+        var isLoading = false
+        var estateId = ""
+        var errorMessage: String?
     }
     
     enum Intent {
+        case onAppear
         
     }
     
     enum SideEffect {
-        
+        case showErrorAlert(String)
     }
     
     func action(_ intent: Intent) {
-        
+        switch intent {
+        case .onAppear:
+            getEstateDetail()
+        }
     }
 }
 
 
 extension EstateDetailStore {
-    
+    private func getEstateDetail() {
+        print("실행 실행")
+        Task {
+            state.isLoading = true
+            
+            defer { state.isLoading = false }
+            
+            do {
+                let estateDetail = try await repository.fetchEstateDetail(estateId: state.estateId)
+                print(estateDetail)
+            } catch let error as NetworkError {
+                effectSubject.send(.showErrorAlert(error.errorDescription))
+            } catch {
+                effectSubject.send(.showErrorAlert(error.localizedDescription))
+            }
+        }
+    }
 }
