f4ad733d83cf99d2d5fdb5ead5ec242f7e1fa450
Maekiee <pdwssf@gmail.com>
2026-02-05 23:22:58 +0900

    채팅 보내기 repository 메서드 추가

--- Cachu/Data/Repositories/ChattingRoomRepository.swift
+++ Cachu/Data/Repositories/ChattingRoomRepository.swift
@@ -27,4 +27,26 @@ extension ChattingRoomRepository: ChattingRoomRepositoryProtocol {
     }
     
     
+    func postSendMessage(roomId: String, message: ChatMessageDTO) async throws -> ChatResponseEntity {
+        let endPoint = ApiEndpoint.postMessage(roomId: roomId, message: message)
+        do {
+            let res = try await apiClient.request(endPoint, type: ChatResponseDTO.self)
+            print("-------- 메세지 전송 성공 --------")
+            print(res)
+            print("-------- ---------- --------")
+            return res.toEntity()
+        } catch let error as NetworkError {
+            print("네트워크 에러:: \(error)")
+            throw error
+        } catch let error as DecodingError {
+            print("디코딩 에러:: \(error)")
+            throw error
+        } catch {
+            print("알수 없는 에러:: \(error)")
+            throw error
+        }
+    }
+    
+    
+    
 }
--- Cachu/Domain/Interfaces/Protocols/ChattingRoomRepositoryProtocol.swift
+++ Cachu/Domain/Interfaces/Protocols/ChattingRoomRepositoryProtocol.swift
@@ -3,4 +3,5 @@ import Foundation
 
 protocol ChattingRoomRepositoryProtocol {
     func getChatMessages(roomId: String, next: String?) async throws -> [ChatResponseEntity]
+    func postSendMessage(roomId: String, message: ChatMessageDTO) async throws -> ChatResponseEntity
 }
